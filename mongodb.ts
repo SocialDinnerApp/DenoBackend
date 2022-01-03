3f129479b0c6893ae238d23c04deb474a8011c0b
ilyasbhm <boehm.ilyas@gmail.com>
2022-01-03 14:20:05 +0100

    cleaned current state of the code

--- mongodb.ts
+++ mongodb.ts
@@ -2,25 +2,11 @@ import { MongoClient } from "https://deno.land/x/mongo@v0.29.0/mod.ts";
 import  password  from "./password.ts"
 
 const client = new MongoClient();
-
-//password in ignore file
-// await client.connect(
-//     "mongodb+srv://deno:testpasswort@denobackend.vgbvb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-//   );
-
-// client.connect("mongodb+srv://deno:testpasswort@denobackend.vgbvb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-
-//const db = client.database('notes');
-
   
-  // Connect using srv url
+// Connect using srv url + password 
 await client.connect(
     `mongodb+srv://deno:${password}@denobackend.vgbvb.mongodb.net/DenoBackend?authMechanism=SCRAM-SHA-1`,
   );
-
-// await client.connect(
-//   'mongodb+srv://deno:testpasswort@denobackend.vgbvb.mongodb.net/DenoBackend?authMechanism=SCRAM-SHA-1',
-// );
 
 const db = client.database('notes');
 
