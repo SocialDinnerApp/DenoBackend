5db238b3994acac702d57ce9f1b50eb1bec234a1
ilyas <ilyas.boeh@curemannheim.de>
2022-01-18 23:56:30 +0100

    feature: implemented function to count organizer revenue

--- src/event/event.ts
+++ src/event/event.ts
@@ -8,9 +8,10 @@ export interface Event {
     time_main: string;
     time_dessert: string;
     city: string;
-    zip_code: Int16Array;
+    fee: number;
+    zip_code: number;
     is_public: boolean;
-    max_participants: Int16Array;
+    max_participants: number;
     registration_deadline: string;
     datetime_created: Date;
     datetime_updated: Date;
