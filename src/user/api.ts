ab82b312c22fa33fc564bdde787db5812524f205
ilyas <ilyas.boeh@curemannheim.de>
2022-01-26 11:37:54 +0100

    feaure: route to get user profil information

--- src/user/api.ts
+++ src/user/api.ts
@@ -132,6 +132,26 @@ const updateUser = async (ctx: Context) => {
     ctx.response.body = {
       user
     }
-}
+  }
 
-export {register, login, logout, updateUser, validateToken}
+    const userInformation = async (ctx: Context) => {
+      const headers: Headers = ctx.request.headers;
+    
+        // to make sure that authorization is not null
+        const authorization = headers.get("Authorization");
+        if (!authorization) {
+          ctx.response.status = 401;
+          return;
+        }
+    
+        const jwt = authorization.split(" ")[1];
+        const payload = await verify(jwt, key);
+        const organizerId = payload._id;
+  
+        const user = await userCollection.find({_id: new Bson.ObjectId(String(organizerId))}, {noCursorTimeout: false}).toArray();
+    
+        ctx.response.body = user
+    }
+
+
+export {register, login, logout, updateUser, userInformation, validateToken}
