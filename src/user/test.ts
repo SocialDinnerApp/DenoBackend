b2a079d2f126f036c8b2b57f4a61c27830ca52f9
Jasmina11 <jpascanovic@gmail.com>
2022-01-26 09:43:07 +0100

    feature: added drafts of unit tests

---
+++ src/user/test.ts
@@ -0,0 +1,21 @@
+
+
+import {register, login, logout, validateToken} from "./api.ts";
+import { delay } from "https://deno.land/std@0.121.0/async/delay.ts";
+import { assertEquals } from "https://deno.land/std@0.97.0/testing/asserts.ts";
+
+
+
+// Test if username exists and redirect via button to register tab
+Deno.test("Fail if username does not exist #fail", async () => {
+    
+  
+  });
+  
+  
+  
+  
+  // Test if all mandatory fields are filled
+  Deno.test("Fail if not all mandatory fields are filled #fail", async () => {
+    
+  });
